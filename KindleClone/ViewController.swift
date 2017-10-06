c2c17019fd097a69606672b9e20436a8fdc76d63
Wadee Sami <wadee@medclue.com>
2017-10-06 11:19:45 +0300

    Initial Commit

---
+++ KindleClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KindleClone
+//
+//  Created by Wadee Sami on 10/6/17.
+//  Copyright © 2017 Wadee AbuZant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
