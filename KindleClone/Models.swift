7fb10f65930c5db5f8c90972e2aadee5a60cc4eb
Wadee Sami <wadee@medclue.com>
2017-10-10 21:13:04 +0300

    initial commit.

---
+++ KindleClone/Models.swift
@@ -0,0 +1,21 @@
+//
+//  Models.swift
+//  KindleClone
+//
+//  Created by Wadee Sami on 10/6/17.
+//  Copyright © 2017 Wadee AbuZant. All rights reserved.
+//
+
+import Foundation
+
+
+class Book{
+
+    var name: String?
+    var auther: String?
+}
+
+class Page{
+    var text: String?
+    var number : Int?
+}
